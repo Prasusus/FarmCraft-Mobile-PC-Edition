d6e81bf277c1ba64a79d548ea77010d29ea1318c
Prasusus <robin.petr.ondrousek@gmail.com>
2026-01-16 18:13:19 +0100

    novejsi sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'farmcraft-v1';
+const CACHE_NAME = 'farmcraft-v2';
 const ASSETS = [
-    './Mobil-1.html',
+    './index.html',
     './manifest.json',
     'https://cdn.pixabay.com/audio/2022/05/27/audio_1808fbf07a.mp3',
     'https://cdn.pixabay.com/animation/2023/06/25/21/56/21-56-06-69_512.gif'
